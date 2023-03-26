e776e8b919c29337365cfc95012aa25f76a3f245
islomaliy <islomaliy74@gmail.com>
2023-03-26 18:16:13 +0500

    #added: getUserRepos service

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,15 @@
 import {request} from './request';
 
 export const getUser = async (username: string) => {
-  console.log('calling');
   return await request({
     method: 'get',
     url: `/users/${username}`,
+  });
+};
+
+export const getUserRepos = async (username: string) => {
+  return await request({
+    method: 'get',
+    url: `/users/${username}/repos`,
   });
 };
