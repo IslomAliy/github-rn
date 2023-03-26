c6222912fafae4c7116153c723275a67a9f16998
islomaliy <islomaliy74@gmail.com>
2023-03-26 18:15:35 +0500

    #added: User screen

--- src/screens/User/index.tsx
+++ src/screens/User/index.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Image, View} from 'react-native';
+import {useRoute} from '@react-navigation/native';
+import {styles} from './styles';
+import {getUserRepos} from '../../services/user';
+import InfoTextWrapper from './InfoTextWrapper';
+import Repositories from '../../components/Repositories';
+
+interface UserRouteParams {
+  userData: {
+    avatar_url: string;
+    name: string;
+    company: string;
+    public_repos: string;
+    followers: string;
+    following: string;
+    login: string;
+  };
+}
 
 const User = () => {
+  const [repos, setRepos] = useState([]);
+  const route = useRoute();
+  const {userData} = route.params as UserRouteParams;
+  useEffect(() => {
+    if (userData.login) {
+      getUserRepos(userData.login)
+        .then(res => {
+          setRepos(res.data);
+          console.log(res);
+        })
+        .catch(err => console.log(err));
+    }
+  }, [userData]);
   return (
-    <View>
-      <Text>User</Text>
+    <View style={styles.scrollView}>
+      <View style={styles.headSection}>
+        <Image
+          source={{uri: userData.avatar_url}}
+          style={styles.userImage}
+          alt="user"
+        />
+        <View style={styles.userInfo}>
+          <InfoTextWrapper
+            info={userData.followers}
+            secondaryText="Followers"
+          />
+          <InfoTextWrapper
+            info={userData.following}
+            secondaryText="Following"
+          />
+          <InfoTextWrapper info={userData.public_repos} secondaryText="Repos" />
+        </View>
+      </View>
+      <Repositories repos={repos} />
     </View>
   );
 };
