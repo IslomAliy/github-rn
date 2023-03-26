a16539a49c221cd5901f59c7998cf9f3fb115214
islomaliy <islomaliy74@gmail.com>
2023-03-26 18:14:46 +0500

    #separated: styles

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,15 @@
 import React, {useEffect, useState, useLayoutEffect} from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {Text, ScrollView} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {NativeSyntheticEvent, TextInputChangeEventData} from 'react-native';
+import {
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+  View,
+} from 'react-native';
 import {getUser} from '../../services/user';
 import useDebounce from '../../hooks/useDebounce';
 import SearchResultCard from '../../components/SearchResultCard';
-
+import {styles} from './styles';
 type userDataProps = {
   login: string;
   avatar_url: string;
@@ -22,9 +26,8 @@ const initialUserData = {
 
 const Home = (): JSX.Element => {
   const navigation = useNavigation();
-  // const [isDataLoaded, setIsDataLoaded] = useState(false);
   const [search, setSearch] = useState('');
-  const debouncedSearch = useDebounce({value: search, delay: 700});
+  const debouncedSearch = useDebounce({value: search, delay: 500});
   const [userData, setUserData] = useState<userDataProps>(initialUserData);
 
   useLayoutEffect(() => {
@@ -44,7 +47,6 @@ const Home = (): JSX.Element => {
   useEffect(() => {
     getUser(debouncedSearch)
       .then(res => {
-        console.log('users', res.status);
         setUserData({...res.data, status: res.status});
       })
       .catch(err => {
@@ -52,38 +54,21 @@ const Home = (): JSX.Element => {
         setUserData(initialUserData);
       });
   }, [debouncedSearch]);
-
-  console.log('userData', userData.status);
-
   return (
-    <SafeAreaView style={styles.content}>
+    <ScrollView
+      contentContainerStyle={styles.content}
+      contentInsetAdjustmentBehavior="automatic">
       {userData.status ? (
-        <SearchResultCard
-          navigation={navigation}
-          name={userData.login}
-          img={userData.avatar_url}
-          company={userData.company}
-        />
+        <SearchResultCard navigation={navigation} userData={userData} />
       ) : search ? (
-        <Text>No user found</Text>
+        <Text style={styles.noUserText}>No user found</Text>
       ) : (
-        <Text>Search user from github</Text>
+        <View style={styles.defaultText}>
+          <Text>Search user from github</Text>
+        </View>
       )}
-    </SafeAreaView>
+    </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  content: {
-    alignItems: 'center',
-    top: 0,
-  },
-  text: {
-    display: 'flex',
-    justifyContent: 'center',
-    color: 'red',
-    fontSize: 32,
-  },
-});
 
 export default Home;
