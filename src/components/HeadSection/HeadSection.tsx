160dc5fd3053bc7b9c3f7b0fafef24f83be7fb8b
islomaliy <islomaliy74@gmail.com>
2023-03-28 16:01:31 +0500

    #added: HeadSection

---
+++ src/components/HeadSection/HeadSection.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Image} from 'react-native';
+import {styles} from '../../screens/User/styles';
+import InfoTextWrapper from '../../screens/User/InfoTextWrapper';
+
+type userDataTypes = {
+  userData: {
+    avatar_url: string;
+    followers: string;
+    following: string;
+    public_repos: string;
+  };
+};
+
+const HeadSection = ({userData}: userDataTypes) => {
+  return (
+    <View style={styles.headSection}>
+      <Image
+        source={{uri: userData.avatar_url}}
+        style={styles.userImage}
+        alt="user"
+      />
+      <View style={styles.userInfo}>
+        <InfoTextWrapper info={userData.followers} secondaryText="Followers" />
+        <InfoTextWrapper info={userData.following} secondaryText="Following" />
+        <InfoTextWrapper info={userData.public_repos} secondaryText="Repos" />
+      </View>
+    </View>
+  );
+};
+
+export default HeadSection;
