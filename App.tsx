a0f1d1295b50ca662edd73b4d376d10d66b99301
islomaliy <islomaliy74@gmail.com>
2023-04-10 17:57:05 +0500

    #added: route types

--- App.tsx
+++ App.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import Home from './src/screens/Home';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import User from './src/screens/User';
+import Home from './src/screens/HomeScreen';
+import {
+  NavigationContainer,
+  NavigationProp,
+  RouteProp,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import User from './src/screens/UserScreen';
+import { RootStackParamList } from './src/types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export type UserProps = {
+  route: RouteProp<RootStackParamList, 'User'>;
+  navigation: NavigationProp<RootStackParamList>;
+};
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{title: 'Home'}} />
-        <Stack.Screen name="User" component={User} options={{title: 'User'}} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen
+          name="User"
+          component={User}
+          options={{ title: 'User' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type RootStackParamList = {
+  Home: undefined;
+  User: {
+    userData: {
+      avatar_url: string;
+      name: string;
+      company: string;
+      public_repos: string;
+      followers: string;
+      following: string;
+      login: string;
+    };
+  };
+};
