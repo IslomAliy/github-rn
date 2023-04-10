2fb72ea8a1d2f703feddcc049a509f81ffefc70c
islomaliy <islomaliy74@gmail.com>
2023-04-10 17:59:20 +0500

    #added: InfoTextWrapper

---
+++ src/screens/UserScreen/InfoTextWrapper.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+type PropTypes = {
+  info: string;
+  secondaryText: string;
+};
+
+const InfoTextWrapper = ({ info, secondaryText }: PropTypes) => {
+  return (
+    <View>
+      <Text style={styles.infoText}>{info}</Text>
+      <Text style={styles.secondaryText}>{secondaryText}</Text>
+    </View>
+  );
+};
+
+export default InfoTextWrapper;
