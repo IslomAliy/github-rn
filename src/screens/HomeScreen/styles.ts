4ecd3dfbb98d368714778bb43e9fe050a6b97efd
islomaliy <islomaliy74@gmail.com>
2023-04-10 17:58:29 +0500

    #added: styles

---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    marginTop: 10,
+    justifyContent: 'center',
+  },
+  text: {
+    justifyContent: 'center',
+    color: 'red',
+    fontSize: 32,
+  },
+  noUserText: {
+    marginTop: 30,
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  defaultText: {
+    marginTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
