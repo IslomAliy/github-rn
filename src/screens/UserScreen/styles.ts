7e8262fafa281100e2c39b9079c9ad4645bd3917
islomaliy <islomaliy74@gmail.com>
2023-04-10 18:00:12 +0500

    #added: styles

---
+++ src/screens/UserScreen/styles.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+  },
+  headSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+    margin: 10,
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 12,
+  },
+  userImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+  },
+  userInfo: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '70%',
+  },
+  infoText: {
+    fontSize: 17,
+    fontWeight: '600',
+  },
+  secondaryText: {
+    color: '#848587',
+    marginTop: 2,
+  },
+});
